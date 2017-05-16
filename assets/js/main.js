a87cc9bf3240a82ae1ea811361d0353ef46f1393
MarionFoto <marionacp@gmail.com>
2017-05-16 15:46:22 -0400

    boton funciona

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+	var boton = document.getElementById("demo");
+	boton.addEventListener("click",hola);
+}
+function hola(){
+	var mensaje = document.getElementById("mensaje");
+	mensaje.innerHTML = "Hola Mundo"
+}
