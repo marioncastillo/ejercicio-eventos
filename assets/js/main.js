8db0183b0ac46ebefdd070267243aae40220ea91
MarionFoto <marionacp@gmail.com>
2017-05-16 23:05:54 -0400

    funciona onmouse y el otro pero no el cargar

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,15 @@ function hola(){
 	var mensaje = document.getElementById("mensaje");
 	mensaje.innerHTML = "HOLA MUNDO"
 }
+
+function cargando(){
+	aler("La pagina esta completamente cargada");
+}
+
+function mouseout(){
+	alert("Estas sobre mi");
+}
+
+function mouseovert(){
+	alert("No estas sobre mi");
+}
